610fab08b31d4d9b093cbc29ba76e03b76552eec
Ivan_A <Ivan_a93@mail.ru>
2020-12-19 15:53:03 +0300

    add efRepository

---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
+{
+    public class EfRepository<T>: 
+        IRepository<T>
+        where T: BaseEntity
+    {
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
