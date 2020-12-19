3399258804536fd5483068a43f3ed3517b5947bb
Ivan_A <Ivan_a93@mail.ru>
2020-12-19 14:25:15 +0300

    add DataContext

---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Data
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
