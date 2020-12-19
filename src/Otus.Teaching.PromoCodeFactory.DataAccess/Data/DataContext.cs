0ba6145c0ce3375f3c7223c5477a1aac06ad2026
Ivan_A <Ivan_a93@mail.ru>
2020-12-19 14:44:58 +0300

    Добавлены сущности

--- src/Otus.Teaching.PromoCodeFactory.DataAccess/Data/DataContext.cs
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Data/DataContext.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
 
 namespace Otus.Teaching.PromoCodeFactory.DataAccess.Data
 {
     public class DataContext: DbContext
     {
         public DbSet<Employee> Employees { get; set; }
+        
+        public DbSet<Role> Roles { get; set; }
+        
+        public DbSet<Customer> Customers { get; set; }
+        
+        public DbSet<Preference> Preferences { get; set; }
+        
+        public DbSet<PromoCode> PromoCodes { get; set; }
     }
 }
