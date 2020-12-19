e7c710ba67bf649184acbf349f436bf162c7df6f
Ivan_A <Ivan_a93@mail.ru>
2020-12-19 13:37:59 +0300

    Перенаправление на сваггер

---
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Otus.Teaching.PromoCodeFactory.WebHost.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [Route(""), HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public RedirectResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger/");
+        }
+    }
+}
--- src/Otus.Teaching.PromoCodeFactory.WebHost/Program.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Program.cs
@@ -13,7 +13,7 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
