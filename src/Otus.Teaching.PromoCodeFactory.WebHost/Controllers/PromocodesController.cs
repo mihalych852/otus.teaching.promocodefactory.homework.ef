dd6a743c93817bbfad52d2529bb60dd8370ec13e
Ivan Azbukin <ivan_a93@mail.ru>
2020-12-21 16:00:49 +0300

    реализован GetPromocodesAsync

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.WebHost.Mappers.PromoCodeMapper;
 using Otus.Teaching.PromoCodeFactory.WebHost.Models;
 
 namespace Otus.Teaching.PromoCodeFactory.WebHost.Controllers
@@ -14,15 +18,34 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost.Controllers
     public class PromocodesController
         : ControllerBase
     {
+        private readonly IRepository<Preference> _preferenceRepository;
+        private readonly IRepository<PromoCode> _promocodeRepository;
+
+        private readonly IPromoCodeMapper _promoCodeMapper;
+
+        public PromocodesController(
+            IRepository<Preference> preferenceRepository,
+            IRepository<PromoCode> promocodeRepository,
+            IPromoCodeMapper promoCodeMapper)
+        {
+            _preferenceRepository = preferenceRepository;
+            _promocodeRepository = promocodeRepository;
+
+            _promoCodeMapper = promoCodeMapper;
+        }
+        
+        
         /// <summary>
         /// Получить все промокоды
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
+        public async Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
         {
-            //TODO: Получить все промокоды 
-            throw new NotImplementedException();
+            var promocodes = await _promocodeRepository.GetAllAsync();
+            var response = promocodes.Select(_promoCodeMapper.ToShortResponse);
+
+            return Ok(response);
         }
         
         /// <summary>
