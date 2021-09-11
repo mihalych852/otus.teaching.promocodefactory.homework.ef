338bc1b312705528fe907db0d604bc334891ab40
Paul Rozhkin <blackiiifox@gmail.com>
2021-09-11 12:12:38 +0300

    remove unused using

--- src/Otus.Teaching.PromoCodeFactory.Core/Domain/PromoCodeManagement/CustomerPreference.cs
+++ src/Otus.Teaching.PromoCodeFactory.Core/Domain/PromoCodeManagement/CustomerPreference.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement
 {
--- src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +8,6 @@ using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using Otus.Teaching.PromoCodeFactory.DataAccess;
-using Otus.Teaching.PromoCodeFactory.DataAccess.Data;
 using Otus.Teaching.PromoCodeFactory.DataAccess.Repositories;
 
 namespace Otus.Teaching.PromoCodeFactory.WebHost
