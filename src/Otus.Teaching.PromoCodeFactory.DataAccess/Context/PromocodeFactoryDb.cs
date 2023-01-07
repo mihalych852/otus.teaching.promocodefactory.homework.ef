67a45cc945bd4859fd58c9ff718e3e0d82f41270
Serjo85 <bobrosebob07@gmail.com>
2023-01-07 19:30:00 +0300

    В DbContext добавлен AutoInclude для навигационных коллекций Customer

--- src/Otus.Teaching.PromoCodeFactory.DataAccess/Context/PromocodeFactoryDb.cs
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Context/PromocodeFactoryDb.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Otus.Teaching.PromoCodeFactory.DataAccess.Context
 {
@@ -13,5 +14,10 @@ namespace Otus.Teaching.PromoCodeFactory.DataAccess.Context
         public virtual DbSet<PromoCode> PromoCodes { get; set; }
         public PromocodeFactoryDb(DbContextOptions<PromocodeFactoryDb> options) : base(options)
         { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().Navigation(e => e.Preferences).AutoInclude();
+            modelBuilder.Entity<Customer>().Navigation(e => e.PromoCodes).AutoInclude();
+        }
     }
 }
