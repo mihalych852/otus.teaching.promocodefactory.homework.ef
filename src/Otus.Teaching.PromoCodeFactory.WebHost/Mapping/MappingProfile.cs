571c089aa03af7cc3a506ef7c06f2a2e0f4cd246
Serjo85 <bobrosebob07@gmail.com>
2023-01-08 03:31:35 +0300

    Добавлен маппинг для GivePromoCodeRequest

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Mapping/MappingProfile.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Mapping/MappingProfile.cs
@@ -20,8 +20,13 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost.Mapping
             CreateMap<Customer, CustomerResponse>();
             CreateMap<CreateOrEditCustomerRequest, Customer>()
                 .ForMember(c => c.Preferences, source => source.MapFrom(
-                    c => c.PreferenceIds.Select(p => new Preference(){Id = p}).ToList()));
+                    c => c.PreferenceIds.Select(p => new Preference { Id = p }).ToList()));
             CreateMap<PromoCode, PromoCodeShortResponse>();
+            CreateMap<GivePromoCodeRequest, PromoCode>()
+                .ForMember(p => p.Code, source => source.MapFrom(
+                g => g.PromoCode))
+                .ForMember(p => p.Preference, source => source.MapFrom(
+                    g => new Preference(){Id = Guid.Parse(g.Preference)}));
             CreateMap<Preference, PreferenceResponse>();
 
         }
